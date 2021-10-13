6ce3f4c1e019fcd470e189ebbede7824991b5cfd
josenaldeOliveira <josenalde@gmail.com>
2021-10-13 20:51:48 -0300

    Update max_min.js

--- src/max_min.js
+++ src/max_min.js
@@ -4,8 +4,9 @@ function getRandom(min, max) {
 
 let a = []; 
 for (let i = 0 ; i < 10 ; i++) { 
-    a.push(getRandom(-9,9)); 
+    a.push(getRandom(-9,9)); //o resultado da função getRandom é inserido no array
 } 
 console.log(a);
 maior = Math.max.apply(null, a); //ou min
+// o método Math.max se aplica para listas de números, como Math.max(1,20,5,10), mas não com objetos array como parâmetros
 console.log(maior)
